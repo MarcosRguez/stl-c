75cc16400e540de338e30bde6ccd641cf2c34573
MarcosRguez <mrodrigue11091@alumno.uned.es>
2026-09-14 19:50:40 +0000

    Añadir archivo de configuración de lanzamiento para depuración en VSCode y optimizar opciones de compilación en CMake

--- src/h/exception.h
+++ src/h/exception.h
@@ -3,34 +3,39 @@
 #include <stdlib.h>
 #include "../h/stack.h"
 typedef struct {
-	jmp_buf* b;
-} jmp;
-typedef vector(jmp) vector_jmp;
-stack(jmp, vector) a;
-typedef struct {
 	const char* what;
 } exception;
-exception exce;
+typedef struct {
+	jmp_buf* b;
+	exception e;
+} contexto;
+typedef vector(contexto) vector_contexto;
+stack(contexto, vector) contextos;
+#if __has_c_attribute(noreturn)
+[[noreturn]]
+#endif
 void throw(const exception e) {
-	if (!stack_empty(&a)) {
-		exce = e;
-		longjmp(*stack_top(&a).b, 0);
+	if (!stack_empty(&contextos)) {
+		stack_top(&contextos).e = e;
+		longjmp(*stack_top(&contextos).b, 2);
 	} else {
 		fprintf(stderr, "exception: %s\n", e.what);
 		abort();
 	}
 }
-#define try                  \
-	{                          \
-		jmp_buf bufo;            \
-		jmp cosa = {.b = &bufo}; \
-		stack_push(&a, cosa);    \
-	}                          \
-	auto _cach = false;        \
-	if (setjmp(*stack_top(&a).b) == 0)
-#define catch(var)       \
-	else { _cach = true; } \
-	var = &exce;           \
-	stack_pop(&a);         \
+#define try                                           \
+	jmp_buf _bufo;                                      \
+	{                                                   \
+		contexto cosa = {.b = &_bufo};                    \
+		stack_push(&contextos, cosa);                     \
+	}                                                   \
+	auto _cach = false;                                 \
+	const auto _jmp = setjmp(*stack_top(&contextos).b); \
+	if (_jmp == 0)
+#define catch(var)                      \
+	else if (_jmp == 1) { _cach = true; } \
+	var = &stack_top(&contextos).e;       \
+	stack_pop(&contextos);                \
 	if (_cach)
-#define finally
+#define finally \
+	else
